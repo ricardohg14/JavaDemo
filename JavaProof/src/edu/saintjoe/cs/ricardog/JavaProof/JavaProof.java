9274a4bc42a5b354c03f6922eb1802d549d5a204
ricardog <ricardog@localhost>
2016-02-08 14:48:00 -0500

    I have conquered this project!!

---
+++ JavaProof/src/edu/saintjoe/cs/ricardog/JavaProof/JavaProof.java
@@ -0,0 +1,11 @@
+package edu.saintjoe.cs.ricardog.JavaProof;
+
+public class JavaProof {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// I have conquered this task!!
+
+	}
+
+}
